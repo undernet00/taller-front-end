7e6cc2062e64516b6c6c35b6216e2e724a7628d2
Martin Rivero <rivero.info@gmail.com>
2024-08-03 14:50:29 -0300

    add icons on Eventos

--- src/Evento.jsx
+++ src/Evento.jsx
@@ -1,14 +1,25 @@
+import { useSelector } from "react-redux";
 import * as Const from "./Constantes";
 
 const Evento = (props) => {
-  const urlImagen = () => {
-    return Const.URL_IMAGENES + 1 + ".png";
+  const categorias = useSelector((state) => state.categorias.categorias);
+
+  const imagenDesdeCategoria = (idCategoriaBuscada) => {
+
+    let categoriaBuscada = categorias.find(
+      (cat) => (cat.id === idCategoriaBuscada)
+    );
+    return categoriaBuscada.imagen;
+  };
+
+  const urlImagen = (idCategoria) => {
+    return Const.URL_IMAGENES + imagenDesdeCategoria(idCategoria) + ".png";
   };
   return (
     <tr>
       <td>{props.id}</td>
       <td>
-        <img src={urlImagen()} />
+        <img src={urlImagen(props.idCategoria)} />
       </td>
       <td>{props.detalle}</td>
       <td>{props.fecha}</td>
