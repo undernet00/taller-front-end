e0cac339a24f72d0d22fb10b9f2f0da21118f694
Martin Rivero <rivero.info@gmail.com>
2024-08-06 06:45:27 -0300

    add logout

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import * as Const from "../Constantes";
+import { useNavigate } from "react-router-dom";
 
 const Menu = () => {
   const [nombreUsuario, setNombreUsuario] = useState("");
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const usuario = window.localStorage.getItem(Const.LOCAL_USUARIO);
@@ -11,28 +14,29 @@ const Menu = () => {
     }
   });
 
-  const handleLogout = ()=>{
+  const handleLogout = () => {
     window.localStorage.setItem(Const.LOCAL_USUARIO, "");
-    window.localStorage.setItem(Const.LOCAL_ID_USUARIO , "");
+    window.localStorage.setItem(Const.LOCAL_ID_USUARIO, "");
     window.localStorage.setItem(Const.LOCAL_API_KEY, "");
     setNombreUsuario("");
-  }
+    navigate("/");
+  };
 
-  if (nombreUsuario!==""){
-  return (
-
-    
-    <div className="menu">
-      <h2>Nav Bar</h2>
-      <p>Logueado como {nombreUsuario}.</p>
-      <br />
-      <button onClick={handleLogout}>Logout</button>
-    </div>
-  )}
-  else {
-    return (<div>
-      <p>Debe iniciar sesión.</p>
-    </div>)
+  if (nombreUsuario !== "") {
+    return (
+      <div className="menu">
+        <h2>Nav Bar</h2>
+        <p>Logueado como {nombreUsuario}.</p>
+        <br />
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>Debe iniciar sesión.</p>
+      </div>
+    );
   }
 };
 
